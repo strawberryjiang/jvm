c78aa7461b2ef6ac969373db16e9652d26b0e7a0
yangjiang <1572467891@qq.com>
2021-01-09 17:52:21 +0800

    chapter1到chapter6

--- chapter01/src/com/atguigu/java/StackStructureTest.java
+++ chapter01/src/com/atguigu/java/StackStructureTest.java
@@ -4,9 +4,9 @@
  * @author shkstart
  * @create 2020 下午 12:11
  */
-public class StackStruTest {
+public class StackStructureTest {
     public static void main(String[] args) {
-        //int i = 2 + 3;
+//        int i = 2 + 3;
         int i = 2;
         int j = 3;
         int k = i + j;
